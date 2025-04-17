585363a9811d908c415ce08049c8835ff884611f
Axel <axel.z24377@gmail.com>
2025-04-17 20:41:09 +0200

    initial project

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <NavBar />
+    <main>
+      <slot />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+// Le composant Layout ne nécessite pas de logique supplémentaire
+</script>
+
+<style scoped>
+main {
+  min-height: calc(100vh - 150px);
+}
+</style> 
